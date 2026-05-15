3bc40917b8edc4eed56fb20f38add81f75a34ccd
its-rohitrawat <rohit98rawat96@gmail.com>
2026-05-15 18:19:03 +0530

    first commit

--- frontend/src/components/UserDashboard.jsx
+++ frontend/src/components/UserDashboard.jsx
@@ -36,7 +36,7 @@ const CATEGORIES = [
   },
   {
     category: "Snacks",
-    image: "https://images.unsplash.com/photo-1699666397768-0126340e880a?q=80&w=1288&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    image: "https://images.unsplash.com/photo-1699666397768-0126340e880a?q=80&w=1288&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D ",
   },
   {
     category: "Main Course",
